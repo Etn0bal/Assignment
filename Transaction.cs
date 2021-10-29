914a9f08f3981e0c01e75eba788506c47fb50e7b
Antoine Labonté <antoinelabo@videotron.ca>
2021-10-29 16:20:46 -0400

    added more properties to transaction

--- Transaction.cs
+++ Transaction.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Collections.Generic;
 
 namespace Assignment {
@@ -7,6 +8,10 @@ namespace Assignment {
         public Dictionary<string, string> Requests { get; set; } = new Dictionary<string, string>();
 
         public string Id { get; set; }
+
+        public List<string> ModifiedValues = new List<string>();
+
+        public TimeSpan TransactionCreatedTime { get; set; }
     }
 }
 
