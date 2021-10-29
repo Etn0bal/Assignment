137680edf6f0f2d9ea06ed55804be3d66936e1a2
Antoine Labonté <antoinelabo@videotron.ca>
2021-10-29 16:20:55 -0400

    added data file

---
+++ Data.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Assignment 
+{
+    public class Data
+    {
+        public string Value { get; set; }
+
+        public TimeSpan LastModifiedTime { get; set; }
+    }
+}
